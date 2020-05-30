f4407426ff3b73b1ec291f6825809db31fcd8d1e
Abdallah <abdallah.yossf@gmail.com>
2020-05-30 18:41:02 +0200

    added support unlimited states and you can name the states whatever you like

--- learning_python.py
+++ learning_python.py
@@ -1,14 +1,7 @@
 import numpy as np
 
-
 n_states = int(input('Enter the number of states: '))
-
-if (n_states > 10):
-   exit()
-
-
 n_inputs = int(input('Enter the number of inputs: '))
-
 
 '''
 rows are states (0 to n_states)
@@ -21,31 +14,34 @@ x=0 x=1
  6   8 
 
 '''
-
+# map states names' to index and vice versa
+state_index = {}
+state_name = {}
 
 next_states = np.zeros((n_states, n_inputs), dtype=np.uint8)
 outputs = np.zeros((n_states, n_inputs), dtype=np.uint8)
-print("States numbered 0 to " + str(n_states) + " has been created")
 
+print("Fill state names table")
+for index in range(n_states):
+    inp = input()
+    state_index[inp] = index
+    state_name[index] = inp
+print(state_name)
+print(state_index)
 
 print("Fill state table")
-for state in range (n_states):
-    for inp in range (n_inputs):
-        print(str(state) + " goes to what? when input is " + str(inp) + " :                     [states start from 0]")
-        next_states[state, inp] = int(input());
-
+for state in range(n_states):
+    for inp in range(n_inputs):
+        print(str(state_name[state]) + " goes to what? when input is " + str(inp))
+        next_states[state, inp] = state_index[input()]
 print(next_states)
 
-
 print("Fill output table")
-for state in range (n_states):
-    for inp in range (n_inputs):
-        print(str(state) + " outputs what? when input is " + str(inp) + " :                     [states start from 0]")
-        outputs[state, inp] = int(input());
-
+for state in range(n_states):
+    for inp in range(n_inputs):
+        print(str(state_name[state]) + " outputs what? when input is " + str(inp))
+        outputs[state, inp] = int(input())
 print(outputs)
-
-
 
 """next_states = np.array
  [[4 4]
@@ -68,28 +64,26 @@ outputs = np.array([[1 1]
  [0 0]
  [1 1]]"""
 
-
-table = np.empty((n_states,  n_states), dtype=object)
-for i in range (n_states):
+table = np.empty((n_states, n_states), dtype=object)
+for i in range(n_states):
     for j in range(n_states):
-        table[i,j] = []
+        table[i, j] = []
 
-
-#different outputs iteration
-for i in range (1, n_states):
+# different outputs iteration
+for i in range(1, n_states):
     for j in range(i):
-        #compare state i's and j's outputs
+        # compare state i's and j's outputs
         for k in range(n_inputs):
             if outputs[i, k] != outputs[j, k]:
-                #different outputs detected , mark it None
-                table[i,j] = None
-                table[j,i] = None
+                # different outputs detected , mark it None
+                table[i, j] = None
+                table[j, i] = None
                 break
 
 # marking requirments iteration
 for i in range(1, n_states):
     for j in range(i):
-        if table[i, j] == None:
+        if table[i, j] is None:
             continue
 
         # compare state i's and j's next states
@@ -97,31 +91,29 @@ for i in range(1, n_states):
             if next_states[i, k] != next_states[j, k]:
                 # different next state detected
                 # print(table[i,j])
-                table[i, j].append(str(next_states[i, k]) + str(next_states[j, k]))
+                table[i, j].append((next_states[i, k], next_states[j, k]))
 
-
-
-for l in range(1000): #effecient yes yes
-    #checking requirments iteration
-    for i in range (1, n_states):
+for l in range(1000):  # efficient yes yes
+    # checking requirements iteration
+    for i in range(1, n_states):
         for j in range(i):
-            if table[i, j] == None:
+            if table[i, j] is None:
                 continue
 
-            #loop over table[i, j] requirements and check if any is impossible
+            # loop over table[i, j] requirements and check if any is impossible
             for req in table[i, j]:
-                if table[ int(req[0]), int(req[1]) ]  == None:
-                    #found an impossible requirement
+                if table[int(req[0]), int(req[1])] is None:
+                    # found an impossible requirement
                     table[i, j] = None
                     table[j, i] = None
 
-#print(table)
+# print(table)
 
-#declaring equivalnet states
+# declaring equivalent states
 for i in range(1, n_states):
     for j in range(i):
-        if table[i, j] == None:
+        if table[i, j] is None:
             continue
-        print(str(i) + " and " + str(j) + " are equivalent")
+        print(state_name[i] + " and " + state_name[j] + " are equivalent")
 
 print("ya")
